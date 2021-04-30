5fa13366e5b99dda38489d638916370e201597c5
Grejo Joby <grejo00@gmail.com>
2021-04-30 18:30:48 +0530

    list all songs from device with name and duration

--- App.js
+++ App.js
@@ -2,24 +2,22 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './app/navigation/AppNavigator';
 import AudioProvider from './app/context/AudioProvider';
-import { SafeAreaView } from 'react-native';
-import AudioListItem from './app/components/audioListItem';
 
 export default function App() {
   return (
-    // <AudioProvider>
-    //   <NavigationContainer>
-    //     <AppNavigator />
-    //   </NavigationContainer>
-    // </AudioProvider>
-    <SafeAreaView>
-      <AudioListItem />
-      <AudioListItem />
-      <AudioListItem />
-      <AudioListItem />
-      <AudioListItem />
-      <AudioListItem />
-    </SafeAreaView>
+    <AudioProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </AudioProvider>
+    // <SafeAreaView>
+    //   <AudioListItem />
+    //   <AudioListItem />
+    //   <AudioListItem />
+    //   <AudioListItem />
+    //   <AudioListItem />
+    //   <AudioListItem />
+    // </SafeAreaView> 
   );
 }
 
