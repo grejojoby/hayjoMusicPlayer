6fa72e8dd58f105172e9649cd88614cc617166fc
Grejo Joby <grejo00@gmail.com>
2021-05-03 15:37:27 +0530

    slider and player basics

--- app/screens/Player.js
+++ app/screens/Player.js
@@ -1,20 +1,64 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import Screen from '../components/Screen'
+import color from '../misc/color';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Slider from '@react-native-community/slider';
+
+const { width } = Dimensions.get('window');
 
 const Player = () => {
     return (
-        <View style={styles.container}>
-            <Text>Player</Text>
-        </View>
-    )
+        <Screen>
+            <View style={styles.container}>
+                <Text style={styles.audioCount}>1 / 99</Text>
+                <View style={styles.midBannerContainer}>
+                    <MaterialCommunityIcons name="music-circle" size={300} color={color.ACTIVE_BG} />
+                </View>
+                <View style={styles.audioPlayerContainer}>
+                    <Text numberOfLines={1} style={styles.audioTitle}>Audio File Name</Text>
+                    <Slider
+                        style={{ width: width, height: 40 }}
+                        minimumValue={0}
+                        maximumValue={1}
+                        minimumTrackTintColor={color.FONT_MEDIUM}
+                        maximumTrackTintColor={color.ACTIVE_BG}
+                    />
+                </View>
+            </View>
+        </Screen>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+
+    },
+    audioCount: {
+        textAlign: 'right',
+        padding: 15,
+        color: color.FONT_LIGHT,
+        fontSize: 14,
+
+    },
+    midBannerContainer: {
+        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
-    }
+        alignItems: 'center',
+
+    },
+    audioPlayerContainer: {
+
+    },
+    audioTitle: {
+        fontSize: 16,
+        color: color.FONT,
+        padding: 15,
+
+    },
+
+
 })
 
 export default Player;
